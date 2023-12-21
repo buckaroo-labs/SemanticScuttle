cf4701a7507c7ddb547e7080669906b4ffe80a83
buckaroo-labs <46908086+buckaroo-labs@users.noreply.github.com>
2023-12-20 22:09:22 -0800

    Update toolbar.inc.php
    Rename toolbar items

--- data/templates/minimal/toolbar.inc.php
+++ data/templates/minimal/toolbar.inc.php
@@ -6,11 +6,11 @@ if ($userservice->isLoggedOn() && is_object($currentUser)) {
 
 	<ul class="nav navbar-nav navigation">
 		<li><a href="<?php echo createURL(''); ?>"><i class="fa fa-home"></i> <?php echo T_('All'); ?></a></li>    
-		<li><a href="<?php echo createURL('bookmarks', $cUsername); ?>"><i class="fa fa-star"></i> <?php echo T_('Bookmarks'); ?></a></li>
+		<li><a href="<?php echo createURL('bookmarks', $cUsername); ?>"><i class="fa fa-star"></i> <?php echo T_('Mine'); ?></a></li>
 		<li><a href="<?php echo createURL('alltags', $cUsername); ?>"><i class="fa fa-tags"></i> <?php echo T_('Tags'); ?></a></li>
 		<li><a href="<?php echo createURL('watchlist', $cUsername); ?>"><i class="fa fa-users"></i> <?php echo T_('Watchlist'); ?></a></li>
 		<li><a href="<?php echo $userservice->getProfileUrl($cUserId, $cUsername); ?>"><i class="fa fa-user"></i> <?php echo T_('Profile'); ?></a></li>
-		<li><a href="<?php echo createURL('bookmarks', $cUsername . '?action=add'); ?>"><i class="fa fa-bookmark"></i> <?php echo T_('Add a Bookmark'); ?></a></li>
+		<li><a href="<?php echo createURL('bookmarks', $cUsername . '?action=add'); ?>"><i class="fa fa-bookmark"></i> <?php echo T_('Add'); ?></a></li>
 		<li><a href="<?php echo createURL('about'); ?>"><i class="fa fa-info-circle"></i> <?php echo T_('About'); ?></a></li>
 	</ul>
 	<ul class="nav navbar-nav navbar-right navigation">
