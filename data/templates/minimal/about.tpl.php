78b989f29004fab78946e8606b6b9a752efcd4dc
buckaroo-labs <heiner@monstro.us>
2023-12-20 21:39:10 -0800

    add 'minimal' theme

---
+++ data/templates/minimal/about.tpl.php
@@ -0,0 +1,31 @@
+<?php
+$this->includeTemplate($GLOBALS['top_include']);
+?>
+
+<div class="container-full">
+	<div class="row">
+
+		<ul>
+		<li><a target="_blank" href="http://sourceforge.net/projects/semanticscuttle/">Semantic Scuttle</a> <?php echo T_('is licensed under the ');?> <a  target="_blank"href="http://www.gnu.org/copyleft/gpl.html"><acronym title="GNU\'s Not Unix">GNU</acronym> General Public License</a> (<?php echo T_('you can freely host it on your own web server'); ?>)
+		<?php echo sprintf(T_(' and supports most of the <a target="_blank" href="http://www.delicious.com/help/api">del.icio.us <abbr title="Application Programming Interface">API</abbr></a>. '), $GLOBALS['sitename']); ?></li>
+		<li><a target="_blank" href="https://github.com/jonrandoem/sscuttlizr/">Sscuttlizr</a> <?php echo T_('is licensed under the ');?> <a target="_blank" href="https://github.com/jonrandoem/sscuttlizr/blob/master/LICENSE">MIT License</a>
+
+
+		<?php if(!is_null($currentUser) && $currentUser->isAdmin()): ?>
+		<li>SemanticScuttle v0.98.5</li>
+		<?php endif ?>
+		</ul>
+
+		<h3><?php echo T_('Tips'); ?></h3>
+		<ul>
+		<li><?php echo T_('Add search plugin into your browser:'); ?> <a href="#" onclick="window.external.AddSearchProvider('<?php echo ROOT?>api/opensearch.php');">opensearch</a></li>
+		<li><?php echo T_('The secret tag "system:unfiled" allows you to find bookmarks without tags.'); ?></li>
+		<li><?php echo T_('The secret tag "system:imported" allows you to find imported bookmarks.'); ?></li>
+		</ul>
+
+	</div>
+</div>
+
+<?php
+$this->includeTemplate($GLOBALS['bottom_include']);
+?>
---
+++ data/templates/minimal/jappix.php
@@ -0,0 +1,82 @@
+<?php
+$show = false;
+
+if ( $currentUser->isAdmin() && isset($GLOBALS['jappixEnableForAdmin']) && $GLOBALS['jappixEnableForAdmin'] == true ) {
+	$show = true;
+} else {
+	if ( $userservice->isLoggedOn() && isset($GLOBALS['jappixEnableForLogged']) && $GLOBALS['jappixEnableForLogged'] == true ) {
+		$show = true;
+	}
+}
+
+
+
+if ( $show == true ) {
+	?>
+	<script type="text/javascript">
+		
+		jQuery.ajaxSetup({cache: true});
+		<?php 
+		$url = "";
+		if ( isset($GLOBALS['jappixUrl']) && $GLOBALS['jappixUrl'] != "" ) {
+			$url .= $GLOBALS['jappixUrl'];
+		} else {
+			$url .= "https://static.jappix.com";
+		}
+		$url .= '/php/get.php?l=';
+		$langs = array('ar', 'bg', 'cs', 'de', 'en', 'eo', 'es', 'et', 'fa', 'fr', 'he', 'hu', 'id', 'it', 'ja', 'la', 'lb', 
+										'mn', 'nl', 'oc', 'pl', 'pt-br', 'pt', 'ru', 'sk', 'sv', 'tr', 'uk', 'zh-cn', 'zh-tw');
+		if ( isset($GLOBALS['jappixLang']) && in_array($GLOBALS['jappixLang'], $langs) ) {
+			$url .= $GLOBALS['jappixLang'];
+		} else {
+			$url .= 'en';
+		}
+		$url .= '&t=js&g=mini.xml';
+
+		$autoconnect = ( isset($GLOBALS['jappixAutoConnect']) && $GLOBALS['jappixAutoConnect'] == true ) ? 'true' : 'false';
+
+		$domain = ( isset($GLOBALS['jappixDomain']) && $GLOBALS['jappixDomain'] != "" ) ? $GLOBALS['jappixDomain'] : 'anonymous.jappix.com';
+
+		$paramStr = $autoconnect . ', true, "' . $domain . '"';
+		if ( isset($GLOBALS['jappixAuth']) && $GLOBALS['jappixAuth'] == true && isset($GLOBALS['jappixUser']) && isset($GLOBALS['jappixPassword']) ) {
+			$paramStr .= ', "' . $GLOBALS['jappixUser'] . '", "' . $GLOBALS['jappixPassword'] . '"';
+		}
+
+		$animate = ( isset($GLOBALS['jappixAnimate']) && $GLOBALS['jappixAnimate'] == true ) ? 'MINI_ANIMATE = true;' : 'MINI_ANIMATE = false;';
+
+		$resource = ( isset($GLOBALS['jappixResource']) && $GLOBALS['jappixResource'] != "" ) ? 'MINI_RESOURCE = "' . $GLOBALS['jappixResource'] . '";' : 'SemanticScuttle';
+
+		$nick = ( isset($GLOBALS['jappixNickName']) && $GLOBALS['jappixNickName'] != '' ) ? 'MINI_RANDNICK = true; MINI_NICKNAME = "' . $GLOBALS['jappixNickName'] . '";' : 'MINI_RANDNICK = true;';
+
+		$error = ( isset($GLOBALS['jappixErrorLink']) && $GLOBALS['jappixErrorLink'] != '' ) ? 'MINI_ERROR_LINK = "' . $GLOBALS['jappixErrorLink'] . '";' : 'MINI_ERROR_LINK = https://mini.jappix.com/issues;';
+
+		$disableMobile = ( isset($GLOBALS['jappixDisableMobile']) && $GLOBALS['jappixDisableMobile'] == true ) ? 'MINI_DISABLE_MOBILE = true;' : 'MINI_DISABLE_MOBILE = false;';
+
+		$groupChats = '';
+		if ( isset($GLOBALS['jappixGroupChats']) && is_array($GLOBALS['jappixGroupChats']) ) {
+			$groupChats .= 'MINI_GROUPCHATS = ["' . implode('", "', $GLOBALS['jappixGroupChats']) . '"];';
+		}
+
+		$title = ( isset($GLOBALS['jappixTitle']) && $GLOBALS['jappixTitle'] != "" ) ? 'MINI_TITLE = "' . $GLOBALS['jappixTitle'] . '";' : 'Chat';
+
+		?>
+		jQuery.getScript("<?php echo $url; ?>", function() {
+			<?php echo $groupChats; ?>
+			<?php echo $animate; ?>
+			<?php echo $nick ?>
+			<?php echo $resource ?>
+			<?php echo $error ?>
+			<?php echo $title ?>
+			<?php echo $disableMobile ?>
+			launchMini(<?php echo $paramStr; ?>);
+			jQuery('#jappix_mini a.jm_pane').css('height', '25px');
+			jQuery('#jappix_mini a.jm_chat-tab, #jappix_mini a.jm_chat-tab.jm_clicked').css('height', '25px');
+			jQuery('#jappix_mini div.jm_actions').css('height', '25px');
+			jQuery('#jappix_mini div.jm_status_picker a, #jappix_mini div.jm_chan_suggest a').css('height', '20px');
+			jQuery('#jappix_mini div.jm_status_picker, #jappix_mini div.jm_chan_suggest').css('top', '24px');
+		});
+		
+	</script>
+	<?php
+}
+?>
